5de5eedbefc7e45891e097e169f2cf4b27adf2cc
Flavio Miceli <miceli@dialogic.nl>
2021-02-23 16:23:42 +0100

    Planning out project

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,26 @@
 
     <div><h1>Weerstation dinges</h1></div>
 
-    <div class="text-converter">
-      <h2> User input dinges.</h2>
-      <form class="text-converter_user-input">
-        <label for="user-input"><strong>"Enter text here"</strong></label>
-        <textarea id="user-input" rows="4"/>
-      </form>
+    <div class="text-weather_wrapper">
+      <!-- Text convert -->
+      <div class="text-converter">
+
+        <!-- Input -->
+        <h2> User input text converter dinges.</h2>
+        <form class="text-converter_user-input">
+          <label for="user-input"><strong>Enter text here</strong></label>
+          <textarea id="user-input" rows="4"/>
+        </form>
+
+        <!-- Output -->
+        <!-- <div> -->
+          <!-- Output field -->
+        <!-- </div> -->
+
+      </div>
+
+      <!-- Weather divs -->
+
     </div>
 
   </div>
@@ -43,6 +57,13 @@ export default defineComponent({
 </script>
 
 <style>
+
+  .text-weather_wrapper {
+    display: grid;
+    grid-template-columns: 2 fr 2 fr;
+    width: 100%;
+    padding: 50px 5%;
+  }
 
   .text-converter {
     display: flex;
