7c0c653a24f638579a56ab14bc16b0eb45bfa9a9
Flavio Miceli <miceli@dialogic.nl>
2021-02-25 10:21:23 +0100

    readme updated, Dialogic style added.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # weerstation
 
+Small project to learn Vue3 and typescript.
+It's a single page with an input field on one side,
+and weather information on the other side.
+
+My focus was on good code, not pretty results.
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,13 @@
         <!-- Input -->
         <h2> User input text converter dinges.</h2>
         <form class="text-converter_user-input">
-          <label for="user-input"><strong>Enter text here</strong></label>
+          <label for="user-input"><h3>Enter text here</h3></label>
           <textarea id="user-input" rows="4" v-model="textFieldInput"/>
         </form>
 
         <!-- Output -->
         <!-- TODO: -->
-        <!-- Button to toggle live encoding. Submit button. CSS. -->
+        <!-- Submit button. CSS. Button to toggle live encoding (and grey out submit button)? -->
         <div class="text-converter_output">
           {{ this.base64encoded }}
         </div>
@@ -95,12 +95,157 @@ export default defineComponent({
     margin: 0;
   }
 
+/* /Dialogic/Formats/Markdown/Typora theme/dialogic.css */
+
+  * {
+    padding: 0px;
+    margin: 0px;
+  }
+
+  body {
+    font-family: verdana;
+    font-size: 9pt;
+  }
+
+  p, li {
+    color: rgb(77, 77, 77);
+  }
+
+  p {
+    text-align: justify;
+    margin-bottom: 8pt;
+    line-height: 1.4em;
+  }
+
+  li > p {
+    margin-bottom: 0pt;
+  }
+
+  p + ul {
+    margin-bottom: 0;
+  }
+
+  li {
+    margin-left: 1cm;
+    margin-bottom: 4pt;
+  }
+
   h1 {
-    margin: 0;
+    margin-top: 12pt;
+    margin-bottom:  8pt;
+    font-size: 22pt;
+    font-weight: bold;
+    color:  rgb(0,55,100);
+    break-after: avoid;
   }
 
   h2 {
-    margin: 0;
+    margin-top: 12pt;
+    font-size: 11pt;
+    margin-bottom: 8pt;
+    font-weight: bold;
+    color:  rgb(0,55,100);
+    break-after: avoid;
+  }
+
+  h3 {
+    font-size: 10pt;
+    margin-top: 12pt;
+    margin-bottom: 8pt;
+    font-style: italic;
+    color:  rgb(0,55,100);
+    font-weight: normal;
+    break-after: avoid;
+  }
+
+  h4 {
+    font-size:  9pt;
+    font-weight: bold;
+    font-style: italic;
+    color: rgb(77, 77, 77);
+    line-height:  1.4em;
+    margin-top: 12pt;
+    break-after: avoid;
+  }
+
+  h5 {
+    font-size:  9pt;
+    font-weight: normal;
+    font-style: italic;
+    color: rgb(77, 77, 77);
+    line-height:  1.4em;
+    margin-top: 12pt;
+    break-after: avoid;
+  }
+
+  a, a:visited {
+    color:  rgb(77, 77, 77);
+  }
+
+  table {
+    margin-top: 4pt;
+    margin-bottom: 8pt;
+  }
+
+  table th {
+    color:  white;
+    background-color: rgb(0,55,100);
+    font-weight:  bold;
+    font-size: 9pt;
+    padding: 4pt;
+    -webkit-print-color-adjust: exact; 
+  }
+
+  table td {
+    font-size:  9pt;
+    color:  rgb(77,77,77);
+    padding: 4pt;
+  }
+
+  table tr:nth-child(2n) td {
+    background-color: #EFEFEF;
+  }
+
+  code, pre {
+    margin-bottom: 8pt;
+    margin-top: 8pt;
+    color:  rgb(77, 77, 77);
+    font-size: 9pt;
+    font-family: consolas, sans-serif;
+    
+  }
+
+  hr {
+    border: none;
+    border-top: solid 1px rgb(77,77,77);
+    margin-bottom: 8pt;
+  }
+
+  blockquote {
+    margin-top: 0.5cm;
+    margin-bottom: 0.5cm;
+    padding: 0.5cm;
+    padding-top: 8pt;
+    padding-bottom: 1pt;
+    background-color: rgba(0,0,0,0.1);
+  }
+
+  ul {
+    margin-top: 2pt;
+  }
+
+  img {
+    max-width: 100%;
+    object-fit: cover;
+  }
+
+  @media print {
+    #write {
+      counter-reset: h1-counter 0 h2-counter 0 h3-counter 0;
+      padding-left: 5.5cm;
+      padding-bottom: 3cm;
+      padding-right:  5.8cm;
+    }
   }
 
 </style>
